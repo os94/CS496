b6fc6d4fcc54b448091c0d3148a00c8109db48dd
os94 <ss1511@naver.com>
2019-02-14 20:06:02 +0900

    -

--- models/account.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require ('mongoose');
-var Schema = mongoose.Schema;
-
-var AccountSchema = new Schema({
-	u_id: String,
-	u_pw: String
-});
-
-module.exports = mongoose.model('accounts', AccountSchema);
--- models/contact.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var ContactSchema = new Schema({name : String, number : String});
-
-module.exports = mongoose.model('contact',ContactSchema);
-
